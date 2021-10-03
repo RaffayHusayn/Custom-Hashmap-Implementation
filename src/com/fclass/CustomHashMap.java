e34e4b47d2841282103fda389f3410259aed2d49
Raffay Syed <raffayhusayn@gmail.com>
2021-10-03 16:55:34 -0400

    Changed CustomHashMap class to calculate Index using bitwise AND

--- src/com/fclass/CustomHashMap.java
+++ src/com/fclass/CustomHashMap.java
@@ -3,19 +3,34 @@ package com.fclass;
 public class CustomHashMap<K,V> {
 
     //Total no of the linked lists this HashMap can have
-    private final int INITIAL_CAPACITY= 16;
+    private int initialCap = 16;
     private Node<K,V>[] bucket ; //this is the array that will be the head node of each eventual linked list
-
+    private int hashIndex;
 
     //An array of 16 is made when no size is mentioned
     public CustomHashMap(){
-        this.bucket = new Node[INITIAL_CAPACITY];
+        this.bucket = new Node[initialCap];
 
     }
 
     //Constructor for custom size array
     public CustomHashMap(int size){
+        this.initialCap = size;
         this.bucket = new Node[size];
     }
 
+    //put method for inserting key value pair in the hashmap
+    public void put(K key, V val){
+    Node<K,V> node = new Node<>(key, val);
+
+
+
+    //IMPORTANT: USING BITWISE AND OPERATOR (&) INSTEAD OF MODULUS TO DEAL WITH NEGATIVE HASHCODE VALUES
+    this.hashIndex = node.hashCode() & (initialCap -1); //this is Bitwise AND operator
+
+    }
+
+    public int getHashIndex() {
+        return hashIndex;
+    }
 }
--- src/com/fclass/Main.java
+++ src/com/fclass/Main.java
@@ -4,5 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        CustomHashMap<Integer, String> head = new CustomHashMap<>(116);
+        head.put(1212123 , "Ali");
+        System.out.println(head.getHashIndex());
     }
 }
