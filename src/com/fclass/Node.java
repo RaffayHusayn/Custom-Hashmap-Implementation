219f31b757cfeae4fdf9b95c15c2ea0761ad6779
Raffay Syed <raffayhusayn@gmail.com>
2021-10-03 16:46:49 -0400

    Override equals() and hashCode() method for Node Class

--- src/com/fclass/Node.java
+++ src/com/fclass/Node.java
@@ -1,14 +1,19 @@
 package com.fclass;
 
+import java.util.Objects;
+
 public class Node<K, V> {
     private final K key;
     private V value;
     private Node<K,V> next;
+    private int hash;
 
     // Constructor
     public Node(K key, V value) {
         this.key = key;
         this.value = value;
+        this.next = null;
+        this.hash = key.hashCode();
     }
 
     //Getter
@@ -22,5 +27,18 @@ public class Node<K, V> {
    //Setter for value but not for Key because key can never change
     public void  setValue(V value){
         this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Node)) return false;
+        Node<?, ?> node = (Node<?, ?>) o;
+        return Objects.equals(getKey(), node.getKey());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getKey());
     }
 }
